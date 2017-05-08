fadbc33afe4ca48d01b9298683a519d187cb7a21
Sharon Zavala <sharon@MacBook-Pro.local>
2017-05-08 14:43:19 -0700

    user null not working

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var keys = require("./keys.js");
+//console.log(keys)
 
 var Twitter = require('twitter');
 
@@ -7,40 +8,45 @@ var spotify = require('spotify');
 var request = require("request");
 
 
-//console.log(keys)
 
- 
-
-switch (action){
-	case "my-tweets":
-	myTweets();
-	break;
-
-	case "spotify-this-song":
-	spotifySong();
-	break;
-
-	case "movie-this":
-	movieThis();
-	break;
-
-	case "do-what-it-says":
-	doThis();
-	break;
-}
-
-//grab input index [2-3]
-var userArgs = process.argv;
-
-var input = ""; 
-
-for (var i = 2; i < userArgs.length; i++) {
-
-  
-  input = input + " " + nodeArgs[i];
-
+//grab arg index for CLI  
+var myArg = process.argv.slice(2);
 
 /*
+console.log('myArg', myArg);
+
+myArg.forEach(function(val, index, array) {
+  console.log(index + " , " + val);
+}); 
+*/
+
+
+var userArg = process.argv.slice(3).join(" ");
+
+//console.log(userArg);
+
+
+var action = process.argv[2];
+
+
+switch (action) {
+    case "my-tweets":
+        myTweets();
+        break;
+
+    case "spotify-this-song":
+        spotifySong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doThis();
+        break;
+}
+/*==========
 ### What Each Command Should Do
 
 1. `node liri.js my-tweets`
@@ -49,17 +55,19 @@ for (var i = 2; i < userArgs.length; i++) {
 
 ===========*/
 
-function myTweets(){
+function myTweets() {
 
-var call = new Twitter(keys.twitterKeys);
 
-var params = {screen_name: 'relicrust'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-  	
-    console.log(tweets);
-  }
-});
+
+    var call = new Twitter(keys.twitterKeys);
+
+    var params = { screen_name: 'relicrust' };
+    call.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+
+            console.log(tweets);
+        }
+    });
 
 
 }
@@ -76,33 +84,36 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
      * "The Sign" by Ace of Base
 
 =======*/
-function spotifySong (){
-
-  if (input === "" || input = null){
-    input = "The Sign";
-  }
 
 
- 
-spotify.search({ type: 'track', query: input }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
+function spotifySong() {
+
+    if (userArg === "" || userArg === null) {
+        userArg = "The Sign";
     }
- 	
-   	for (var i = 0; i < data.tracks.items.length; i++) {
-   		//console.log(data.tracks.items[i])
-   		if(data.tracks.items[i].album.album_type === "album"){
-   			index = i; 
-   			console.log("Artists: " + data.tracks.items[index].album.artists[0].name);
-            console.log("Song's track: " + data.tracks.items[index].name);
-            console.log("Preview link: " + data.tracks.items[index].preview_url);
-            console.log("Album title: " + data.tracks.items[index].album.name);
-            return;	
-        }
-}
 
-});
+
+
+
+    spotify.search({ type: 'track', query: userArg }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            //console.log(data.tracks.items[i])
+            if (data.tracks.items[i].album.album_type === "album") {
+                index = i;
+                console.log("Artists: " + data.tracks.items[index].album.artists[0].name);
+                console.log("Song's track: " + data.tracks.items[index].name);
+                console.log("Preview link: " + data.tracks.items[index].preview_url);
+                console.log("Album title: " + data.tracks.items[index].album.name);
+                return;
+            }
+        }
+
+    });
 }
 
 /*=====
@@ -126,25 +137,25 @@ spotify.search({ type: 'track', query: input }, function(err, data) {
      * It's on Netflix!
 =========*/
 
-function movieThis(){
+function movieThis() {
 
-if (input === null || input === ""){
-	input === "Mr.Nobody";
-}
-  request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&r=json", function(error, response, body) {
- 
-         
-         if (!error && response.statusCode === 200) {
-          
-             console.log("Title: " + JSON.parse(body).Title);
-             console.log("Released Date: " + JSON.parse(body).Released);
-             console.log("Rating: " + JSON.parse(body).imdbRating);
-             console.log("Country produced in: " + JSON.parse(body).Country);
-             console.log("Language: " + JSON.parse(body).Language);
-             console.log("Short Summary: " + JSON.parse(body).Plot);
-             console.log("Actors: " + JSON.parse(body).Actors);
-             console.log("URL: http://www.imdb.com/title/" + JSON.parse(body).imdbID);
-         }
+    if (userArg === null || userArg === "") {
+        userArg === "Mr.Nobody";
+    }
+    request("http://www.omdbapi.com/?t=" + userArg + "&y=&plot=short&r=json", function(error, response, body) {
+
+
+        if (!error && response.statusCode === 200) {
+
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Released Date: " + JSON.parse(body).Released);
+            console.log("Rating: " + JSON.parse(body).imdbRating);
+            console.log("Country produced in: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Short Summary: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("URL: http://www.imdb.com/title/" + JSON.parse(body).imdbID);
+        }
     });
 
 }
@@ -155,18 +166,20 @@ if (input === null || input === ""){
      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
      * Feel free to change the text in that document to test out the feature for other commands.
 
-     */
-function doThis(){
-
-var fs = require("fs");
-
-fs.readFile("random.txt", "utf8", function(error, data){
-	var dataArr = data.spit(",");
-	input = dataArr[0];
-	submit = dataArr.slice(1).join(" ");
+===========*/
 
 
-});
+function doThis() {
 
-}//close doThis function 
-}//close input for loop
+    var fs = require("fs");
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        var dataArr = data.split(" , ");
+        input = dataArr[0];
+        submit = dataArr.slice(1).join(" ");
+
+
+    });
+
+
+} //close doThis function
